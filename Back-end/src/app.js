870b55ff9a731a26a1c762acff63b9bd3a235b93
antoniohdz117 <antonio117.berrios@gmail.com>
2026-05-01 23:49:30 -0600

    Descomenta inserts de asignacion curso

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -7,12 +7,11 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
 
-// 
+
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(express.json());
 
-// 
 app.get("/", (req, res) => {
   res.render("index");
 });
